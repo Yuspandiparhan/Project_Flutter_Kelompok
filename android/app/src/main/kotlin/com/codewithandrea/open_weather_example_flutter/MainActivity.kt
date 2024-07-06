f198df8cc8367a955f3257cf5aa5e80242462732
Parhan Yuspandi <parhan.ypd@gmail.com>
2024-07-07 02:55:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/codewithandrea/open_weather_example_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codewithandrea.weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const rainBlueLight = Color.fromARGB(255, 242, 3, 218);
+  static const rainBlueDark = Color.fromARGB(255, 245, 189, 5);
+  static const rainGradient = [rainBlueLight, rainBlueDark];
+  static const accentColor = Color(0xFFe96e50);
+}
